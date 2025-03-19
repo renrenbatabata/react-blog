c4a3c5b3544ac4e2b7202c82d140b86d200dcb18
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-03-19 19:24:22 +0900

    Add files via upload

---
+++ page.js
@@ -0,0 +1,21 @@
+
+const Home = () =>{
+
+
+  return(
+    <>
+    <header>HEADER</header>
+
+    <main>
+      <div>
+        <h1>CUBE</h1>
+        <p>アウトプットしていくサイト</p>
+      </div>
+    </main>
+
+    <footer>FOOTER</footer>
+    </>
+)
+}
+
+export default Home;
