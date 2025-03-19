23795a22722076077020d41a167f5e32e751ced3
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-03-19 20:57:34 +0900

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import Hero from "@/components/hero";
+
+const Home = () =>{
+
+
+  return(
+   <Layout>
+    <Hero/>
+   </Layout>
+)
+}
+
+export default Home;
